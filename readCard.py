aaef1ac9410975f93af99ce37e2effa22baff791
Dan <dcard@umich.edu>
2022-04-04 23:32:48 -0400

    Changes to readCard.py

--- readCard.py
+++ readCard.py
@@ -1,12 +1,12 @@
-#!/usr/bin/env python
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 reader = SimpleMFRC522()
-try:
-    print("Waiting for you to scan an RFID sticker/card")
-    id = reader.read()[0]
-    print("The ID for this card is:", id)
+while True:
+    try:
+        print("Waiting for you to scan an RFID sticker/card")
+        id = reader.read()[0] # Takes the first value of the tuple
+        print("The ID for this card is:", id)
 
-finally:
-    GPIO.cleanup()
+    finally:
+        GPIO.cleanup()
